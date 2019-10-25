3d18cf119ce2a294d8ad75b6c83d65ecf8f549ed
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-10-25 15:43:41 -0700

    Abstracted List Of Routes out

--- React-Frontend/starter-kit/src/App.jsx
+++ React-Frontend/starter-kit/src/App.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import NavagationBar from "./components/Shared/NavagationBar.jsx";
-import {Route} from "react-router-dom";
 
-import PublicIndex from "./components/public/PublicIndex";
-import Login from "./util/auth/Login"
-
-// Imports components
-// import ListOfRoutes from './util/ListOfRoutes.js';
+// Imports List Of Routes so this file doesnt get cluttered with future expansion
+import ListOfRoutes from './util/ListOfRoutes.js';
 
 function App() {
   return (
     <div className="App">
       <NavagationBar />
-      <Route exact path="/" component={PublicIndex} />
-			<Route path="/login" component={Login} />
       Main App Lives Here
+      <ListOfRoutes />
     </div>
   );
 }
