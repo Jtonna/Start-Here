2eea6a2804355eefc060514a4b146c8c38b52e67
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-10-25 14:33:06 -0700

    Setup a 404 page fro react-router

---
+++ React-Frontend/starter-kit/src/components/Shared/error404.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const error404 = () => {
+	return (
+		<div className="error404">
+			OOPS you found a page that doesnt exist
+		</div>
+	);
+}
+ 
+export default 404;
