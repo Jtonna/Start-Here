be33fadbd61401621083894b8fe8fc6d04fda59d
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-10-30 11:52:57 -0700

    WIP

---
+++ React-Frontend/starter-kit/src/util/reducers/AuthenticationReducer.js
@@ -0,0 +1,9 @@
+// We need to import our actions first
+
+// Initialize State
+
+// What we return depending on the actions
+
+export const AuthenticationReducer = () => {
+	
+}
---
+++ React-Frontend/starter-kit/src/util/reducers/RootReducer.js
@@ -0,0 +1,10 @@
+// combineReducers allows to to combine all of our reducers into one file for easier state management of the app becomes large and complex
+// https://redux.js.org/api/combinereducers
+import {combineReducers} from 'redux';
+
+// Import all of the reducers here
+import AuthenticationReducer from './AuthenticationReducer';
+
+export default combineReducers({
+	
+})
