a42b572923d08ae94239a71eb41b6da4d9ff5f54
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-11-05 23:20:13 -0800

    Modifying local login state, using copies each time it updates. Displaying state changes live below the form

--- React-Frontend/starter-kit/src/util/auth/Login.jsx
+++ React-Frontend/starter-kit/src/util/auth/Login.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Login = () => {
-	return (
-		<div className="login">
-			Login Page lives here
-		</div>
-	);
+// This Login component handles user input and saves it to a local state, on submit it will update the global redux state tree which handles the API call
+
+class Login extends Component {
+	// Here we set a local state to be used onSubmit
+	state = {
+		userLoginInfo: {
+			emailOrUsername: '',
+			password: '',
+		}
+	}
+
+	// function that takes in parameters as 'e'
+	handleInput = (e) => {
+		// we are going to need to set local state
+		this.setState({
+			userLoginInfo: {
+				// ...this.state.userLoginInfo creates a copy of the state everything we set the state
+				// https://github.com/tc39/proposal-object-rest-spread
+				... this.state.userLoginInfo,
+				// [e.target.name] is going to get the 'name' of the field we are entering text from
+				// e.target.value gets the value each time we enter in data
+				[e.target.name]: e.target.value
+			}
+		})
+	}
+
+
+	render(){
+		return (
+			<div className="login">
+				Login Page lives here
+				<form action="">
+					<input type="text" name="emailOrUsername" id="" onChange={this.handleInput} />
+					<input type="password" name="password" id="" onChange={this.handleInput} />
+					<input type="submit" name="submit" id="" onClick={this.handleSubmit} />
+
+				</form>
+				<h3>{this.state.userLoginInfo.emailOrUsername}</h3>
+				<h3>{this.state.userLoginInfo.password}</h3>
+			</div>
+		);
+	}
 }
  
 export default Login;
