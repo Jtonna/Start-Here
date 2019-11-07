98fb1989dc8be89f83e5da53f7fc1b0edd832e32
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-11-06 20:46:56 -0800

    Added a test authentication URL and cleaning up  the " to '

--- React-Frontend/starter-kit/src/util/AxiosWithAuth.js
+++ React-Frontend/starter-kit/src/util/AxiosWithAuth.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 // Here we define our URL for Connecting To Online Service's (CTOS) (basically the database we want to access)
-export const CTOS_URL = ""
+export const CTOS_URL = 'https://artportfoliobw.herokuapp.com'
 
 export const AxiosWithAuth = () => {
 	// We need to get the token from localStorage to do things like restoring their last session if they close the page out
 	// https://blog.logrocket.com/the-complete-guide-to-using-localstorage-in-javascript-apps-ba44edb53a36/
-	const token = localStorage.getItem("token");
+	const token = localStorage.getItem('token');
 
 	return axios.create({
 		headers : {
