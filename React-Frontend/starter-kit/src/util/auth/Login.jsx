49b6d95901c4499e0e008291ad30c89e044e8495
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-11-06 16:39:17 -0800

    Fixed spread operator, created a plan for passing in an email or username to the global state for authentication

--- React-Frontend/starter-kit/src/util/auth/Login.jsx
+++ React-Frontend/starter-kit/src/util/auth/Login.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 // This Login component handles user input and saves it to a local state, on submit it will update the global redux state tree which handles the API call
 
@@ -18,9 +19,9 @@ class Login extends Component {
 			userLoginInfo: {
 				// ...this.state.userLoginInfo creates a copy of the state everything we set the state
 				// https://github.com/tc39/proposal-object-rest-spread
-				... this.state.userLoginInfo,
 				// [e.target.name] is going to get the 'name' of the field we are entering text from
 				// e.target.value gets the value each time we enter in data
+				...this.state.userLoginInfo,
 				[e.target.name]: e.target.value
 			}
 		})
@@ -43,5 +44,15 @@ class Login extends Component {
 		);
 	}
 }
- 
-export default Login;
+
+const mapStateToProps = (state) => {
+	// To make it easier on the backend we are going to loop through the state object's emailOrUsername key:value pair (but only the value)
+	// The time complexity is 
+	// if this.state.userLoginInfo.emailOrUsername contains "@"
+	// return email & password
+	// else
+	// return username & password
+}
+
+
+export default connect(mapStateToProps, {})(Login);
