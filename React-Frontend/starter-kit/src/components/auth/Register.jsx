a8b5152457643cb35689d2487611a0808833b02b
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2019-11-12 23:45:13 -0800

    Fixed local state problem, adds id's, comments, connects to the redux store

--- React-Frontend/starter-kit/src/components/auth/Register.jsx
+++ React-Frontend/starter-kit/src/components/auth/Register.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { registration } from '../../util/actions/AuthenticationActions'
+
+import {connect} from 'react-redux';
 
 class Register extends Component {
 	
@@ -9,6 +12,7 @@ class Register extends Component {
 			lname: '',
 			username: '',
 			email: '',
+			password: '',
 		}
 	}
 
@@ -26,23 +30,27 @@ class Register extends Component {
 		})
 	}
 
-	register = (e) => {
+	handleRegister = (e) => {
+		// This basically prevents the submit button from re-freshing the page
 		e.preventDefault();
-		this.props.register(this.state.userRegistrationInfo).then( () => {
-			this.props.history.push('/UserHome')
+		// this.props.(whatever action from redux we need to use)
+		this.props.registration(this.state.userRegistrationInfo)
+		.then( () => {
+			this.props.history.push('/RouteChooser')
 		})
 	}
 
 	render() { 
 		return (
 			<div className="registration">
-				<form className="registration_form" onSubmit={this.register}>
-					<input type="firstname" name="fname" id="" onChange={this.handleInput} />
-					<input type="lastname" name="lname" id="" onChange={this.handleInput} />
-					<input type="username" name="username" id="" onChange={this.handleInput} />
-					<input type="email" name="email" id="" onChange={this.handleInput} />
-					<input type="password" name="password" id="" onChange={this.handleInput} />
-					<input type="submit" name="submit" id="" onChange={this.register} />
+				Registeration page lives here
+				<form className="registration_form" onSubmit={this.handleRegister}>
+					<input type="firstname" name="fname" id="register_fname" onChange={this.handleInput} />
+					<input type="lastname" name="lname" id="register_lname" onChange={this.handleInput} />
+					<input type="username" name="username" id="register_username" onChange={this.handleInput} />
+					<input type="email" name="email" id="register_email" onChange={this.handleInput} />
+					<input type="password" name="password" id="register_password" onChange={this.handleInput} />
+					<input type="submit" name="submit" id="register_submit" onChange={this.handleRegister} />
 				</form>
 				<h3>{this.state.userRegistrationInfo.fname}</h3>
 				<h3>{this.state.userRegistrationInfo.lname}</h3>
@@ -53,5 +61,11 @@ class Register extends Component {
 		);
 	}
 }
- 
-export default Register;
+
+// this will map the state to props (redux store)
+const mapStateToProps = (state) => ({
+	registering: state.registering
+})
+
+// connect (state to props), action's, then export the component
+export default connect(mapStateToProps, {registration})(Register);
